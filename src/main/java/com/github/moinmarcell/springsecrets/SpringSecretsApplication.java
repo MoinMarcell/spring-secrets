b6aeec2784157087c82515c17a2bfbcef654ac32
MoinMarcell <marcell.dechant@protonmail.com>
2023-02-25 01:00:11 +0100

    Project init

--- src/main/java/com/github/moinmarcell/springsecrets/SpringSecretsApplication.java
+++ src/main/java/com/github/moinmarcell/springsecrets/SpringSecretsApplication.java
@@ -2,10 +2,12 @@ package com.github.moinmarcell.springsecrets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 @EnableConfigurationProperties(GitHubConfigProperties.class)
+
 public class SpringSecretsApplication {
 
     public static void main(String[] args) {
